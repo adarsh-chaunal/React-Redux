7df622e5a8dc1b6c2d2e4db66cf34ba0be9ee1db
adarsh-chaunal <er.adarsh97@gmail.com>
2025-04-01 03:36:04 +0530

    Multiple reducers and combining them

--- Readme.md
+++ Readme.md
@@ -67,3 +67,6 @@ is same as
 ```store.dispatch(orderCake());```
 ```store.dispatch(restockCake(3));```
 
+## combineReducers(reducers)
+The ```combineReducers``` helper function turns an object whose values are different "slice reducer" functions into a single combined reducer function you can pass to Redux Toolkit's ```configureStore``` (or the legacy ```createStore``` method)
+> When your app increased in size, you can split reducers into seperate files and keep them completely independent and managing different features. Like, authReducer, userReducer, profileReducer, etc.
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // import { redux } from 'redux'
-const redux = require("redux")
-const createStore = redux.createStore//redux.configureStore
-const bindActionCreators = redux.bindActionCreators
+const redux = require("redux");
+const createStore = redux.createStore; //redux.configureStore
+const bindActionCreators = redux.bindActionCreators;
+const combineReducers = redux.combineReducers;
 
-
-const CAKE_ORDERED = "CAKE_ORDERED"
-const CAKE_RESTOCKED = "CAKE_RESTOCKED"
+const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
 
 function orderCake() {
   return {
@@ -14,37 +16,110 @@ function orderCake() {
   };
 }
 
-function restockCake(qty = 1){
-    return {
-        type: CAKE_RESTOCKED,
-        payload: qty,
-    }
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  };
 }
 
-const initialState = {
-  numOfCake: 10,
+function orderIceCream(qty = 1) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+
+function restockIceCream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+}
+
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIceCreams: 20,
+// }
+
+const initialCakeState = {
+  numOfCakes: 10,
+};
+
+const initialIceCreamState = {
+  numOfIceCreams: 20,
 };
 
 // (previousState, action) => newState
 
-const reducer = (state = initialState, action) => {
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case CAKE_ORDERED:
+//       return {
+//         ...state,
+//         numOfCakes: state.numOfCakes - 1,
+//       };
+//     case CAKE_RESTOCKED:
+//       return {
+//         ...state,
+//         numOfCakes: state.numOfCakes + action.payload,
+//       };
+//     case ICECREAM_ORDERED:
+//       return {
+//         ...state,
+//         numOfIceCreams: state.numOfIceCreams - 1,
+//       };
+//     case ICECREAM_RESTOCKED:
+//       return {
+//         ...state,
+//         numOfIceCreams: state.numOfIceCreams + action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return {
         ...state,
-        numOfCake: state.numOfCake - 1,
-      }
+        numOfCakes: state.numOfCakes - 1,
+      };
     case CAKE_RESTOCKED:
-        return {
-            ...state,
-            numOfCake: state.numOfCake + action.payload,
-        }
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
     default:
-      return state
+      return state;
   }
 };
 
-const store = createStore(reducer);
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+// const store = createStore(reducer);
+const store = createStore(rootReducer);
 console.log("Initial State ", store.getState());
 
 const unsubscribe = store.subscribe(() =>
@@ -57,18 +132,36 @@ const unsubscribe = store.subscribe(() =>
 // store.dispatch(restockCake(3));
 // store.dispatch(orderCake());
 
-const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch) 
-actions.orderCake()
-actions.orderCake()
-actions.orderCake()
-actions.restockCake(3)
+const actions = bindActionCreators(
+  { orderCake, restockCake, orderIceCream, restockIceCream },
+  store.dispatch
+);
+actions.orderCake();
+actions.orderCake();
+actions.orderCake();
+actions.restockCake(3);
+actions.orderIceCream();
+actions.orderIceCream();
+actions.restockIceCream(2);
 
 unsubscribe();
 
+// Result without combineReducer:
+// Initial State  { numOfCakes: 10, numOfIceCreams: 20 }
+// Updated State  { numOfCakes: 9, numOfIceCreams: 20 }
+// Updated State  { numOfCakes: 8, numOfIceCreams: 20 }
+// Updated State  { numOfCakes: 7, numOfIceCreams: 20 }
+// Updated State  { numOfCakes: 10, numOfIceCreams: 20 }
+// Updated State  { numOfCakes: 10, numOfIceCreams: 19 }
+// Updated State  { numOfCakes: 10, numOfIceCreams: 18 }
+// Updated State  { numOfCakes: 10, numOfIceCreams: 20 }
 
-// Result:
-// Initial State  { numOfCake: 10 }
-// Updated State  { numOfCake: 9 }
-// Updated State  { numOfCake: 8 }
-// Updated State  { numOfCake: 7 }
-// Updated State  { numOfCake: 10 }
+// Result with combineReducer:
+// Initial State  { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 20 } }
+// Updated State  { cake: { numOfCakes: 9 }, iceCream: { numOfIceCreams: 20 } }
+// Updated State  { cake: { numOfCakes: 8 }, iceCream: { numOfIceCreams: 20 } }
+// Updated State  { cake: { numOfCakes: 7 }, iceCream: { numOfIceCreams: 20 } }
+// Updated State  { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 20 } }        
+// Updated State  { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 19 } }        
+// Updated State  { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 18 } }        
+// Updated State  { cake: { numOfCakes: 10 }, iceCream: { numOfIceCreams: 20 } } 
